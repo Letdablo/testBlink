ab7309848c4c0300e48580843a064d02698448c9
Your Name <you@example.com>
2023-10-05 12:55:37 +0200

    refactor component superButton

---
+++ src/components/superButton.jsx
@@ -0,0 +1,8 @@
+
+export default function SuperButton({text}) {
+  return (
+    <>
+       <button className='custom-button'> <div className='button-inner'><span className='button-text'>{text}</span></div></button>
+    </>
+  )
+}
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,10 +1,11 @@
+import SuperButton from '../components/superButton';
 import '../css/Layout.scss';
 
 export default function Layout({children}) {
   return (
     <>
         {children}
-        <button className='custom-button'> <div className='button-inner'><span className='button-text'>checkout</span></div></button>
+       <SuperButton text={'checkout'}></SuperButton>
     </>
   )
 }
